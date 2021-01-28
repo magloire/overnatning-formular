b410899f1953c814a849d584e1b36bc56cafd248
Magloire <magloire1@gmail.com>
2021-01-28 21:33:19 +0100

    Updates

---
+++ precache-manifest.37a848f7736af3bf30e4100fb7b6f684.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4a80c288abd5724950a1d4eb038e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4664b4a1547d2a71a36",
+    "url": "/static/css/main.d3a536c3.chunk.css"
+  },
+  {
+    "revision": "6c7f25f9b120f2941c27",
+    "url": "/static/js/2.4040b611.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.4040b611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86839e5420df484fb8dd",
+    "url": "/static/js/3.84ef2c32.chunk.js"
+  },
+  {
+    "revision": "d4664b4a1547d2a71a36",
+    "url": "/static/js/main.f18dbbae.chunk.js"
+  },
+  {
+    "revision": "80a534eac7791e6885c1",
+    "url": "/static/js/runtime-main.b709b1e6.js"
+  }
+]);
