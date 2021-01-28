61928ae75d62a676ca5eaaafa9684550b9a6b47f
Magloire <magloire1@gmail.com>
2021-01-28 21:57:04 +0100

    Updates

---
+++ precache-manifest.656d7a08d93ec2e1d333f074bc7a28f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15bb745c8656a00431cd95f8fe3fac2",
+    "url": "/overnatning-formular/index.html"
+  },
+  {
+    "revision": "d4664b4a1547d2a71a36",
+    "url": "/overnatning-formular/static/css/main.d3a536c3.chunk.css"
+  },
+  {
+    "revision": "6c7f25f9b120f2941c27",
+    "url": "/overnatning-formular/static/js/2.4040b611.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/overnatning-formular/static/js/2.4040b611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86839e5420df484fb8dd",
+    "url": "/overnatning-formular/static/js/3.84ef2c32.chunk.js"
+  },
+  {
+    "revision": "d4664b4a1547d2a71a36",
+    "url": "/overnatning-formular/static/js/main.f18dbbae.chunk.js"
+  },
+  {
+    "revision": "f46689bad871271569d0",
+    "url": "/overnatning-formular/static/js/runtime-main.22ed9674.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/formular/precache-manifest.4fee63be9b34391d2c592230b1a61def.js"
+  "/overnatning-formular/precache-manifest.656d7a08d93ec2e1d333f074bc7a28f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/formular/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/overnatning-formular/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
